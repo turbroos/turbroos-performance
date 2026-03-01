ca2fef95aa7725e0e65cf0af9126352e3b9c7233
turbroos <tubaziofabio@gmx.de>
2026-03-01 17:02:37 +0100

    lang.js

--- js/lang.js
+++ js/lang.js
@@ -1,16 +1,1 @@
-/* 
-  Sprachumschaltung DE/EN wurde bewusst deaktiviert.
-  Alle Inhalte werden statisch in Deutsch dargestellt.
-  Diese Datei existiert nur noch, um JS-Fehler zu vermeiden.
-*/
 
-(function () {
-  document.addEventListener("DOMContentLoaded", () => {
-    document.documentElement.lang = "de";
-  });
-
-  // Dummy-Funktion, falls irgendwo noch onclick="setLang(...)"
-  window.setLang = function () {
-    return false;
-  };
-})();
